9ee2c288d27a5e3aec22d1a3f9538bf523548e10
f4qihuddin <f4qihuddin@gmail.com>
2026-06-18 16:22:25 +0700

    fix yaml file

--- modelling.py
+++ modelling.py
@@ -8,9 +8,12 @@ import warnings
 import sys
 
 if __name__ == "__main__":
+    os.environ["MLFLOW_ALLOW_FILE_STORE"] = "true"
+    tracking_uri = os.path.join(os.path.dirname(os.path.abspath(__file__)), "mlruns")
+    mlflow.set_tracking_uri(f"file:{tracking_uri}")
+    print(f"MLflow tracking URI: {mlflow.get_tracking_uri()}")
     warnings.filterwarnings("ignore")
     np.random.seed(40)
-
     # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
     file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "train_pca.csv")
     data = pd.read_csv(file_path)
@@ -22,21 +25,17 @@ if __name__ == "__main__":
     random_state=42,
     test_size=0.2
     )
-    input_example = X_train[0:5]
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 3
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 3
     with mlflow.start_run():
         model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
         model.fit(X_train, y_train)
-
-        predicted_qualities = model.predict(X_test)
-
-        mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        input_example=input_example
-        )
-        model.fit(X_train, y_train)
-        # Log metrics
+        
         accuracy = model.score(X_test, y_test)
         mlflow.log_metric("accuracy", accuracy)
+        
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model",
+            input_example=X_train.iloc[0:5]
+        )
